ae60576517fa8380c9bed432b528dafbd4874cf0
zxy <1623190186@qq..com>
2024-10-04 10:51:58 +0800

    feat重构路由结构

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -9,27 +9,10 @@ import {
   useLocation,
 } from "react-router-dom";
 import { HomePage, LoginPage } from "./pages";
-import { Menu } from "antd";
-function AppWrapper() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<App></App>} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
 function App() {
-  const location = useLocation(); // 获取当前路由信息
-
-  // 计算 needHeader，根据当前路径是否为 "/login"
-  const needHeader = useMemo(
-    () => location.pathname !== "/login",
-    [location.pathname]
-  );
-  return (
+  const layoutDom = (
     <>
-      <ELayout needHeader={needHeader}>
+      <ELayout>
         <div className="app-content">
           <aside className="layout-main-side">
             <SideMenu />
@@ -37,9 +20,9 @@ function App() {
           <div className="layout-main-content">
             <Routes>
               <Route path="/" element={<Navigate to="/login" replace />} />
-              <Route path="/login" element={<LoginPage />} />
+
               <Route path="/home" element={<HomePage />} />
-              {/* 捕获未知路径，重定向到登录页或主页 */}
+
               <Route path="*" element={<Navigate to="/login" replace />} />
             </Routes>
           </div>
@@ -47,6 +30,14 @@ function App() {
       </ELayout>
     </>
   );
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/*" element={layoutDom}></Route>
+      </Routes>
+    </>
+  );
 }
 
-export default AppWrapper;
+export default App;
